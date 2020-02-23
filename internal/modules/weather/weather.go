c998ca432d119bd119e786161f23f997573ee104
Ray Antonius <rayantonius@gmail.com>
2020-02-23 23:23:34 +0700

    Create querylocation function

--- internal/modules/weather/weather.go
+++ internal/modules/weather/weather.go
@@ -13,45 +13,53 @@ import (
 
 var def Repository
 
+// Repository holds the package module's available functions
 type Repository interface {
+	QueryLocation(locationID int64) (Data, error)
 }
 
-func (w *weatherRepo) testAPI() error {
-	requestURL := fmt.Sprintf("%sid=%d&APPID=%s", weatherURL, jakartaLocationID, w.APIKey)
+// QueryLocation gets the weather data of a city based on locationID
+func (w *weatherRepo) QueryLocation(locationID int64) (Data, error) {
+	var data Data
+	requestURL := fmt.Sprintf("%sid=%d&APPID=%s", weatherURL, locationID, w.APIKey)
 	req, err := http.NewRequest("GET", requestURL, nil)
 	if err != nil {
-		return fmt.Errorf("Module Internal Error, %s", err.Error())
+		return data, fmt.Errorf("Module Internal Error, %s", err.Error())
 	}
 	client := &http.Client{}
-	rctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
+	rctx, cancel := context.WithTimeout(context.Background(), time.Second*1)
 	defer cancel()
 
 	req = req.WithContext(rctx)
 
 	resp, err := client.Do(req)
 	if err != nil {
-		return fmt.Errorf("Module Internal Error, %s", err.Error())
+		return data, fmt.Errorf("Module Internal Error, %s", err.Error())
 	}
 	defer resp.Body.Close()
 
-	var data Data
-
 	err = json.NewDecoder(resp.Body).Decode(&data)
 	if err != nil {
-		return fmt.Errorf("Module Internal Error, %s", err.Error())
+		return data, fmt.Errorf("Module Internal Error, %s", err.Error())
 	}
 	if data.Response != 200 {
-		return fmt.Errorf("API Error, %s", data.Message)
+		return data, fmt.Errorf("API Error, %s", data.Message)
 	}
 
-	return nil
+	return data, nil
 }
 
+// QueryLocation is the global level of the function
+func QueryLocation(locationID int64) (Data, error) {
+	return def.QueryLocation(locationID)
+}
+
+// Init sets up the default variable to handle package functions
 func Init(config configs.WeatherConfig) error {
 	newRepo := &weatherRepo{
 		APIKey: config.APIKey,
 	}
-	err := newRepo.testAPI()
+	_, err := newRepo.QueryLocation(jakartaLocationID)
 	if err != nil {
 		return fmt.Errorf("[Weather][Init] Error initalizing the weather module, err: %s", err.Error())
 	}
