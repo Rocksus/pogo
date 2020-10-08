0e9e640cffe9b511f5e04dc82ec28ec23651c734
Nicky Logan <laurentiusdominick@yahoo.com>
2020-10-08 23:56:07 +0700

    feat: add intent handling

--- go.mod
+++ go.mod
@@ -5,7 +5,7 @@ go 1.13
 require (
 	github.com/joho/godotenv v1.3.0
 	github.com/line/line-bot-sdk-go v7.1.0+incompatible
-	github.com/mitchellh/mapstructure v1.3.3 // indirect
+	github.com/mitchellh/mapstructure v1.3.3
 	github.com/nickylogan/go-log v0.0.0-20201005184020-1cb19c4c0979
 	github.com/wit-ai/wit-go v1.0.8
 	golang.org/x/sys v0.0.0-20201005172224-997123666555 // indirect
--- internal/usecase/replier/message.go
+++ internal/usecase/replier/message.go
@@ -4,7 +4,10 @@ import (
 	"context"
 	"fmt"
 	"reflect"
+	"sort"
 
+	"github.com/Rocksus/pogo/internal/modules/joke"
+	"github.com/Rocksus/pogo/internal/modules/weather"
 	"github.com/Rocksus/pogo/internal/repositories/interpreter"
 	"github.com/line/line-bot-sdk-go/linebot"
 	"github.com/nickylogan/go-log"
@@ -32,7 +35,7 @@ func (m *messageReplier) Reply(ctx context.Context, message linebot.Message) (re
 	case *linebot.StickerMessage:
 		reply, err = m.handleStickerMessage(ctx, msg)
 	default:
-		err = fmt.Errorf("unhandled message type: %s", getMessageType(message))
+		err = fmt.Errorf("unhandled message type: %s", m.getMessageType(message))
 		log.Errorln(err)
 	}
 	if err != nil {
@@ -49,12 +52,33 @@ func (m *messageReplier) handleTextMessage(ctx context.Context, msg *linebot.Tex
 		return
 	}
 
-	// TODO: do something with data later
-	_ = data
-
-	// For now, just echo back the incoming message
-	reply = linebot.NewTextMessage(msg.Text)
-	return
+	intent := m.getBestIntent(data)
+	// TODO: use map of handlers later. This is just a temporary logic
+	switch intent.Name {
+	case "tellJoke":
+		j, err := joke.GetRandomJoke()
+		if err != nil {
+			return nil, err
+		}
+		jokeStr := fmt.Sprintf("%s\n\n%s", j.Setup, j.Punchline)
+		return linebot.NewTextMessage(jokeStr), nil
+	case "weather/checkWeather":
+		w, err := weather.QueryLocation("jakarta")
+		if err != nil {
+			return nil, err
+		}
+		weatherStr := fmt.Sprintf(
+			"Here's the weather in %s, %s:\n\n%s\nHumidity: %d\nTemperature: %.2f°C",
+			w.Name,
+			w.System.Country,
+			w.Weather[0].Description,
+			w.Details.Humidity,
+			w.Details.TemperatureCelcius,
+		)
+		return linebot.NewTextMessage(weatherStr), nil
+	default:
+		return nil, fmt.Errorf("unhandled intent: %s", intent.Name)
+	}
 }
 
 func (m *messageReplier) handleStickerMessage(ctx context.Context, msg *linebot.StickerMessage) (reply linebot.SendingMessage, err error) {
@@ -67,11 +91,25 @@ func (m *messageReplier) handleStickerMessage(ctx context.Context, msg *linebot.
 	return linebot.NewTextMessage(replyText), nil
 }
 
+func (m *messageReplier) getBestIntent(resp interpreter.Response) interpreter.Intent {
+	if len(resp.Intents) == 0 {
+		return interpreter.Intent{}
+	}
+
+	intents := make([]interpreter.Intent, len(resp.Intents))
+	copy(intents, resp.Intents)
+	sort.Slice(intents, func(i, j int) bool {
+		return intents[i].Confidence > intents[j].Confidence
+	})
+
+	return intents[0]
+}
+
 func (m *messageReplier) createDefaultReply() linebot.SendingMessage {
 	return linebot.NewTextMessage("Sorry, I don't quite get that.")
 }
 
-func getMessageType(msg linebot.Message) string {
+func (m *messageReplier) getMessageType(msg linebot.Message) string {
 	t := reflect.TypeOf(msg)
 	if t.Kind() == reflect.Ptr {
 		return t.Elem().Name()
