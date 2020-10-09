6012438b19dd397bf5c908f7f42fc5ef60365f27
Nicky Logan <laurentiusdominick@yahoo.com>
2020-10-10 06:58:07 +0700

    feat: add notifier repository

---
+++ internal/repositories/pushnotif/line/line.go
@@ -0,0 +1,21 @@
+package line
+
+import (
+	"context"
+
+	"github.com/Rocksus/pogo/internal/repositories/pushnotif"
+	"github.com/line/line-bot-sdk-go/linebot"
+)
+
+type pushNotifier struct {
+	client *linebot.Client
+}
+
+func NewPushNotifier(client *linebot.Client) pushnotif.Notifier {
+	return &pushNotifier{client: client}
+}
+
+func (p *pushNotifier) PushMessages(ctx context.Context, to string, messages ...linebot.SendingMessage) error {
+	_, err := p.client.PushMessage(to, messages...).WithContext(ctx).Do()
+	return err
+}
